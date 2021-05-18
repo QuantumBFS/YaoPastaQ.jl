12c0f38d9274dff0869962e74d8c343de5802bde
Documenter.jl <documenter@juliadocs.github.io>
2021-05-18 23:35:00 +0000

    build based on 45de326

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = YaoPastaQ","category":"page"},{"location":"#YaoPastaQ","page":"Home","title":"YaoPastaQ","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [YaoPastaQ]","category":"page"}]
+}
