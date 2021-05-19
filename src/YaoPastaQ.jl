74cf22b1801ba533c872fea3cab849938dd809d2
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2021-05-19 23:25:29 +0530

    Update YaoPastaQ.jl

--- src/YaoPastaQ.jl
+++ src/YaoPastaQ.jl
@@ -26,24 +26,37 @@ genlist!(plist, blk::YGate, locs, controls) = push!(plist, ("Y", locs[1]))
 genlist!(plist, blk::HGate, locs, controls) = push!(plist, ("H", locs[1]))
 genlist!(plist, blk::ZGate, locs, controls) = push!(plist, ("Z", locs[1]))
 genlist!(plist, blk::Scale{Val{im}, 1, YGate}, locs, controls) = push!(plist, ("iY", locs[1]))
-genlist!(plist, blk::TGate, locs, controls) = push!(plist, ("π/8", locs[1]))
+genlist!(plist, blk::TGate, locs, controls) = push!(plist, ("π/8", locs[1])) 
 
-function genlist!(plist, blk::ShiftGate{Float64}, locs, controls) 
+function genlist!(plist, blk::ShiftGate{Float64}, locs, controls)
     if blk.theta == π/2
         push!(plist, ("Phase", locs[1]))
     elseif blk.theta == π/4
         push!(plist, ("π/8", locs[1]))
     end
+end 
+
+genlist!(plist, blk::I2Gate, locs, controls) = push!(plist, ("Id", locs[1])) 
+genlist!(plist, blk::RotationGate{1, Float64, XGate}, locs, controls) = push!(plist, ("Rx", locs[1], (θ = blk.theta,))) 
+genlist!(plist, blk::RotationGate{1, Float64, YGate}, locs, controls) = push!(plist, ("Ry", locs[1], (θ = blk.theta,))) 
+genlist!(plist, blk::RotationGate{1, Float64, ZGate}, locs, controls) = push!(plist, ("Rz", locs[1], (ϕ = blk.theta,))) 
+
+function genlist!(plist, blk::ControlBlock{N, XGate, 1, 1}, locs, controls) where N
+    push!(plist, ("CX", (blk.ctrl_locs[1], blk.locs[1])))
 end
 
-genlist!(plist, blk::I2Gate, locs, controls) = push!(plist, ("Id", locs[1]))
-genlist!(plist, blk::RotationGate{1, Float64, XGate}, locs, controls) = push!(plist, ("Rx", locs[1], (θ = blk.theta,)))
-genlist!(plist, blk::RotationGate{1, Float64, YGate}, locs, controls) = push!(plist, ("Ry", locs[1], (θ = blk.theta,)))
-genlist!(plist, blk::RotationGate{1, Float64, ZGate}, locs, controls) = push!(plist, ("Rz", locs[1], (ϕ = blk.theta,)))
-genlist!(plist, blk::ControlBlock{2, XGate, 1, 1}, locs, controls) = push!(plist, ("CX", (blk.ctrl_locs[1], blk.locs[1])))
-genlist!(plist, blk::ControlBlock{2, YGate, 1, 1}, locs, controls) = push!(plist, ("CY", (blk.ctrl_locs[1], blk.locs[1])))
-genlist!(plist, blk::ControlBlock{2, ZGate, 1, 1}, locs, controls) = push!(plist, ("CZ", (blk.ctrl_locs[1], blk.locs[1])))
-genlist!(plist, blk::ControlBlock{2, RotationGate{1, Float64, ZGate}, 1, 1}, locs, controls) = push!(plist, ("CRz", (blk.ctrl_locs[1], blk.locs[1]), (ϕ = blk.content.theta,)))
+function genlist!(plist, blk::ControlBlock{N, YGate, 1, 1}, locs, controls) where N
+    push!(plist, ("CY", (blk.ctrl_locs[1], blk.locs[1])))
+end
+
+function genlist!(plist, blk::ControlBlock{N, ZGate, 1, 1}, locs, controls) where N
+    push!(plist, ("CZ", (blk.ctrl_locs[1], blk.locs[1])))
+end
+
+function genlist!(plist, blk::ControlBlock{N, RotationGate{1, Float64, ZGate}, 1, 1}, locs, controls) where N
+    push!(plist, ("CRz", (blk.ctrl_locs[1], blk.locs[1]), (ϕ = blk.content.theta,)))
+end
+
 genlist!(plist, blk::SWAPGate, locs, controls) = push!(plist, ("SWAP", (locs[1], locs[2])))
 genlist!(plist, blk::ControlBlock{3, XGate, 2, 1}, locs, controls) = push!(plist, ("Toffoli", (blk.ctrl_locs[1], blk.ctrl_locs[2], blk.locs[1])))
 genlist!(plist, blk::ControlBlock{3, SWAPGate, 1, 2}, locs, controls) = push!(plist, ("Fredkin", (blk.ctrl_locs[1], blk.locs[1], blk.locs[2])))
