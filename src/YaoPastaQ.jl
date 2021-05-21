013e9d0b9da6c2fb98fa261cb98a34245ef877a2
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2021-05-22 00:41:59 +0530

    Update YaoPastaQ.jl

--- src/YaoPastaQ.jl
+++ src/YaoPastaQ.jl
@@ -1,7 +1,7 @@
 module YaoPastaQ
 
 using YaoBase, YaoBlocks, PastaQ
-export genlist, YaoBase.apply!, PastaQReg
+export genlist, apply!, PastaQReg
 flblock(blk::AbstractBlock) = YaoBlocks.Optimise.simplify(blk, rules=[YaoBlocks.Optimise.to_basictypes])
 sublocs(subs, locs) = [locs[i] for i in subs]   
 
