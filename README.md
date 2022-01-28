f0da820e6dc21067a2740343bf45b8e0b93dbb37
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2022-01-28 06:56:01 +0530

    Add docstring for `genlist` function and installation/usage instructions in README.md (#17)
    * Added new gates, fixed the docs
    
    * Replaced the PastaQReg "convenience function" with `create_reg`
    
    * Added more consistent README.md
    
    * Added the tests

--- README.md
+++ README.md
@@ -4,3 +4,58 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://QuantumBFS.github.io/YaoPastaQ.jl/dev)
 [![Build Status](https://github.com/QuantumBFS/YaoPastaQ.jl/workflows/CI/badge.svg)](https://github.com/QuantumBFS/YaoPastaQ.jl/actions)
 [![Coverage](https://codecov.io/gh/QuantumBFS/YaoPastaQ.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/QuantumBFS/YaoPastaQ.jl)
+
+[Yao](https://github.com/QuantumBFS/Yao.jl) and [PastaQ](https://github.com/GTorlai/PastaQ.jl) integration library.
+
+## Installation
+
+<p>
+YaoPastaQ is a &nbsp;
+    <a href="https://julialang.org">
+        <img src="https://raw.githubusercontent.com/JuliaLang/julia-logo-graphics/master/images/julia.ico" width="16em">
+        Julia Language
+    </a>
+    &nbsp; package. To install YaoPastaQ,
+    please <a href="https://docs.julialang.org/en/v1/manual/getting-started/">open
+    Julia's interactive session (known as REPL)</a> and press <kbd>]</kbd> key in the REPL to use the package mode, then type the following command
+</p>
+
+```julia
+pkg> add https://github.com/QuantumBFS/YaoPastaQ.jl
+```
+## Usage
+
+This package provides a PastaQ register called `PastaQReg` and some functions to work with it. 
+
+You can create a PastaQReg using
+
+```julia
+julia> using YaoPastaQ, Yao
+
+julia> p = chain(3, put(1=>X), repeat(H,1:3))
+nqubits: 3
+chain
+├─ put on (1)
+│  └─ X
+└─ repeat on (1, 2, 3)
+   └─ H
+
+julia> apply!(create_reg(3), p)
+PastaQReg{ITensors.MPS}
+    active qubits: 3/3
+```
+
+You can also use the `genlist` function, which can convert a block in Yao to a list which can be read and used by the functions of [PastaQ](https://github.com/GTorlai/PastaQ.jl)
+```julia
+julia> list = genlist(chain(3, put(1=>X), repeat(Rz(π/3),1:3)))
+4-element Vector{Any}:
+ ("X", 1)
+ ("Rz", 1, (ϕ = 1.0471975511965976,))
+ ("Rz", 2, (ϕ = 1.0471975511965976,))
+ ("Rz", 3, (ϕ = 1.0471975511965976,))
+```
+
+## License
+
+MIT License
+
--- src/YaoPastaQ.jl
+++ src/YaoPastaQ.jl
@@ -1,10 +1,24 @@
 module YaoPastaQ
 
 using YaoBase, YaoBlocks, PastaQ
-export genlist, apply!, PastaQReg
+export genlist, apply!, PastaQReg, create_reg
 flblock(blk::AbstractBlock) = YaoBlocks.Optimise.simplify(blk, rules=[YaoBlocks.Optimise.to_basictypes])
 sublocs(subs, locs) = [locs[i] for i in subs]   
 
+"""
+genlist(x::AbstractBlock) -> Vector{Any}
+
+Returns the vector of gates and their locations, from the AbstractBlock.
+
+# Example
+
+```julia
+julia> genlist(chain(2, put(1=>X), put(2=>Y)))
+2-element Vector{Any}:
+ ("X", 1)
+ ("Y", 2)
+```
+"""
 function genlist(x::AbstractBlock{N}) where N
     plist = []
     genlist!(plist, flblock(x), [1:N...], Int[])
@@ -27,15 +41,8 @@ genlist!(plist, ::HGate, locs, controls) = push!(plist, ("H", locs[1]))
 genlist!(plist, ::ZGate, locs, controls) = push!(plist, ("Z", locs[1]))
 genlist!(plist, ::Scale{Val{im}, 1, YGate}, locs, controls) = push!(plist, ("iY", locs[1]))
 genlist!(plist, ::TGate, locs, controls) = push!(plist, ("π/8", locs[1]))
-genlist!(plist, ::ConstGate.SGate, locs, controls) = push!(plist, ("Phase", locs[1])) 
-
-function genlist!(plist, blk::ShiftGate{Float64}, locs, controls)
-    if blk.theta == π/2
-        push!(plist, ("Phase", locs[1]))
-    elseif blk.theta == π/4
-        push!(plist, ("π/8", locs[1]))
-    end
-end 
+genlist!(plist, ::ConstGate.SGate, locs, controls) = push!(plist, ("S", locs[1]))
+genlist!(plist, blk::ShiftGate{Float64}, locs, controls) = push!(plist, ("Phase", locs[1], (ϕ = blk.theta,)))
 
 genlist!(plist, blk::I2Gate, locs, controls) = push!(plist, ("Id", locs[1])) 
 genlist!(plist, blk::RotationGate{1, Float64, XGate}, locs, controls) = push!(plist, ("Rx", locs[1], (θ = blk.theta,))) 
@@ -58,6 +65,18 @@ function genlist!(plist, blk::ControlBlock{N, RotationGate{1, Float64, ZGate}, 1
     push!(plist, ("CRz", (blk.ctrl_locs[1], blk.locs[1]), (ϕ = blk.content.theta,)))
 end
 
+function genlist!(plist, blk::ControlBlock{N, ShiftGate{Float64}, 1, 1}, locs, controls) where N
+    push!(plist, ("CPHASE", (blk.ctrl_locs[1], blk.locs[1]), (ϕ = blk.content.theta,)))
+end
+
+function genlist!(plist, blk::ControlBlock{N, RotationGate{1, Float64, XGate}, 1, 1}, locs, controls) where N
+    push!(plist, ("CRx", (blk.ctrl_locs[1], blk.locs[1]), (ϕ = blk.content.theta,)))
+end
+
+function genlist!(plist, blk::ControlBlock{N, RotationGate{1, Float64, YGate}, 1, 1}, locs, controls) where N
+    push!(plist, ("CRy", (blk.ctrl_locs[1], blk.locs[1]), (ϕ = blk.content.theta,)))
+end
+
 genlist!(plist, blk::SWAPGate, locs, controls) = push!(plist, ("SWAP", (locs[1], locs[2])))
 genlist!(plist, blk::ControlBlock{3, XGate, 2, 1}, locs, controls) = push!(plist, ("Toffoli", (blk.ctrl_locs[1], blk.ctrl_locs[2], blk.locs[1])))
 genlist!(plist, blk::ControlBlock{3, SWAPGate, 1, 2}, locs, controls) = push!(plist, ("Fredkin", (blk.ctrl_locs[1], blk.locs[1], blk.locs[2])))
@@ -67,7 +86,12 @@ mutable struct PastaQReg{State <: Union{PastaQ.MPS, PastaQ.MPO}} <: AbstractRegi
     state::State
 end
 
-PastaQReg(x::Int64) = PastaQReg(productstate(x))
+"""
+create_reg(x::Int64) -> PastaQReg
+
+Create a PastaQReg using `PastaQ.productstate(N::Int)`
+"""
+create_reg(x::Int64) = PastaQReg(productstate(x))
 
 function YaoBase.apply!(r::PastaQReg, x::AbstractBlock)
     r.state = runcircuit(r.state, genlist(x))
@@ -76,11 +100,25 @@ end
 
 YaoBase.nqubits(r::PastaQReg) = length(r.state)
 YaoBase.nactive(r::PastaQReg) = YaoBase.nqubits(r)
-PastaQReg(x::YaoBlocks.BitStr) = PastaQReg(productstate(length(x), reverse(["$i" for i in x])))
-PastaQReg(x::Union{PastaQ.MPS, PastaQ.MPO}) = PastaQReg(productstate(x))
+
+"""
+create_reg(x::YaoBlocks.BitStr) -> PastaQReg
+
+Create a PastaQReg using `PastaQ.productstate(states::Vector{T})`
+"""
+create_reg(x::YaoBlocks.BitStr) = PastaQReg(productstate(length(x), reverse(["$i" for i in x])))
+
+"""
+create_reg(x::Union{PastaQ.MPS, PastaQ.MPO}) -> PastaQReg
+
+Create a PastaQReg using `PastaQ.productstate(x::Union{PastaQ.MPS, PastaQ.MPO})`
+"""
+create_reg(x::Union{PastaQ.MPS, PastaQ.MPO}) = PastaQReg(productstate(x))
+
+
 Base.copy(r::PastaQReg) = PastaQReg(r)
-PastaQReg(r::PastaQReg) = PastaQReg(copy(r.state))
 YaoBase.fidelity(x::PastaQReg, y::PastaQReg) = PastaQ.fidelity(x.state, y.state)
+create_reg(r::PastaQReg) = PastaQReg(copy(r.state))
 
 function YaoBase.measure(x::PastaQReg, nshots::Int=1024)
     return getsamples(x.state, nshots)
